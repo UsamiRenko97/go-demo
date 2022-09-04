f1a822ee9abfcb0c6b7cc908b2ff4142c84b0a44
yuanhaopeng <yuanhaopeng@bytedance.com>
2022-09-04 16:07:55 +0800

    init

---
+++ mit/6.824/src/main/mrcoordinator.go
@@ -0,0 +1,26 @@
+package main
+
+//
+// start the coordinator process, which is implemented
+// in ../mr/coordinator.go
+//
+// go run mrcoordinator.go pg*.txt
+//
+// Please do not change this file.
+//
+
+import (
+	"map-reduce/mit/6.824/src/mr"
+	"time"
+)
+import "os"
+
+func main() {
+
+	m := mr.MakeCoordinator(os.Args[1:], 10)
+	for m.Done() == false {
+		time.Sleep(time.Second)
+	}
+
+	time.Sleep(time.Second)
+}
